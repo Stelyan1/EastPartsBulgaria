5827710dd889ccda975bb962e104c2f1148ca563
Stelyan <Stelyan@BLIZNAKOV1>
2025-04-29 21:11:07 +0300

    Created first controllers for entities and index.cshtmls for testing purposes

---
+++ EastPartsBulgaria/Controllers/CarBrandController.cs
@@ -0,0 +1,21 @@
+﻿using EastParts.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EastPartsBulgaria.Controllers
+{
+    public class CarBrandController : Controller
+    {
+        private readonly EastPartsDbContext dbContext;
+        public CarBrandController(EastPartsDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            var carBrands = this.dbContext.CarBrands.ToList();
+            return View("~/Views/CarBrand/Index.cshtml", carBrands);
+        }
+    }
+}
---
+++ EastPartsBulgaria/Controllers/CarBrandModelsController.cs
@@ -0,0 +1,22 @@
+﻿using EastParts.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace EastPartsBulgaria.Controllers
+{
+    public class CarBrandModelsController : Controller
+    {
+        private readonly EastPartsDbContext dbContext;
+        public CarBrandModelsController(EastPartsDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            var carBrandModels = this.dbContext.CarModels.Include(cbm => cbm.CarBrand).ToList();
+            return View("~/Views/CarBrandModels/Index.cshtml", carBrandModels);
+        }
+    }
+}
